309ba4e4a478a8734268444467a08063481a2ce1
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-09-19 20:53:29 +0900

    Represent snowflake using a class

--- src/index.js
+++ src/index.js
@@ -70,12 +70,6 @@ document.querySelector("#rewind").addEventListener("click", () => {
   player.requestPlay();
 });
 
-/*
- * @name Snowflakes
- * @description Particle system simulating the motion of falling snowflakes.
- * Uses an array of objects to hold the snowflake particles.
- * Contributed by Aatish Bhatia.
- */
 
 let snowflakes = []; // array to hold snowflake objects
 // p5.js を初期化
@@ -87,19 +81,19 @@ new P5((p5) => {
   const numChars = 10;
   const textAreaWidth = width - margin * 2;
 
-  // snowflake class
-  function snowflake() {
-    // initialize coordinates
-    this.posX = 0;
-    this.posY = p5.random(-50, 0);
-    this.initialangle = p5.random(0, 2 * Math.PI);
-    this.size = p5.random(2, 5);
+  class FallingObject {
+    constructor() {
+      // initialize coordinates
+      this.posX = 0;
+      this.posY = p5.random(-50, 0);
+      this.initialangle = p5.random(0, 2 * Math.PI);
+      this.size = p5.random(2, 5);
 
-    // radius of snowflake spiral
-    // chosen so the snowflakes are uniformly spread out in area
-    this.radius = Math.sqrt(p5.random(Math.pow(width / 2, 2)));
-
-    this.update = function(time) {
+      // radius of snowflake spiral
+      // chosen so the snowflakes are uniformly spread out in area
+      this.radius = Math.sqrt(p5.random(Math.pow(width / 2, 2)));
+    }
+    update(time) {
       // x position follows a circle
       let w = 0.1; // angular speed
       let angle = w * time + this.initialangle;
@@ -107,20 +101,37 @@ new P5((p5) => {
 
       // different size snowflakes fall at slightly different y speeds
       this.posY += Math.pow(this.size, 0.5);
+    }
 
-      // delete snowflake if past end of screen
-      if (this.posY > height) {
-        let index = snowflakes.indexOf(this);
-        snowflakes.splice(index, 1);
-      }
-    };
+    y() {
+      return this.posY < height;
+    }
+  }
 
-    this.display = function() {
+  // snowflake class
+  class SnowFlake extends FallingObject {
+    display() {
       p5.fill(p5.color(255, 255, 255));
       p5.noStroke();
       p5.ellipse(this.posX, this.posY, this.size);
-    };
+    }
   }
+
+  class Character extends FallingObject {
+    constructor(text) {
+      super();
+      console.log("showing ".concat(text));
+      this.text = text;
+      this.size = p5.random(20, 50);  // TODO this should be passed as the constructor argument
+    }
+
+    display() {
+      p5.fill(255, 255, 255);
+      p5.textSize(this.size);
+      p5.text(this.text, this.posX, this.posY);
+    }
+  }
+
   // キャンバスを作成
   p5.setup = () => {
     p5.createCanvas(width, height);
@@ -144,14 +155,19 @@ new P5((p5) => {
 
     // create a random number of snowflakes each frame
     for (let i = 0; i < p5.random(5); i++) {
-      snowflakes.push(new snowflake()); // append snowflake object
+      snowflakes.push(new SnowFlake()); // append snowflake object
     }
 
-    let t = p5.frameCount / 60; // update time
+    let time = p5.frameCount / 60; // update time
     // loop through snowflakes with a for..of loop
     for (let flake of snowflakes) {
-      flake.update(t); // update snowflake position
+      flake.update(time); // update snowflake position
       flake.display(); // draw snowflake
+      // delete snowflake if past end of screen
+      if (flake.y() > height) {
+        let index = snowflakes.indexOf(flake);
+        snowflakes.splice(index, 1);
+      }
     }
 
     const position = player.timer.position;  // current playback position
@@ -159,9 +175,7 @@ new P5((p5) => {
     // - 再生位置より 100 [ms] 前の時点での発声文字を取得
     // - { loose: true } にすることで発声中でなければ一つ後ろの文字を取得
     let char = player.video.findChar(position - 100, { loose: true });
-
     if (char) {
-      // 位置決めのため、文字が歌詞全体で何番目かも取得しておく
       let index = player.video.findIndex(char);
 
       while (char) {
