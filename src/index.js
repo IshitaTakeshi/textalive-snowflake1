dbfad0cf5da1da398a6db1665fc790b65b06f521
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-10-10 18:13:32 +0900

    Move particle classes to 'particle.js'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import P5 from "p5";
 import { Player, Ease } from "textalive-app-api";
+import { SnowFlake, Character } from "/particle.js";
 
 // キャンバスの大きさなどを計算
 const width = window.innerWidth; // Math.min(800, window.innerWidth);
@@ -72,60 +73,6 @@ player.addListener({
 
 let snowflakes = []; // array to hold snowflake objects
 
-// This class refers to the snowflake example
-// https://p5js.org/examples/simulate-snowflakes.html
-// The original sample code is provided under the CC BY-NC-SA 4.0 license.
-// https://creativecommons.org/licenses/by-nc-sa/4.0/
-class FallingObject {
-  constructor(p5) {
-    // initialize coordinates
-    this.p5 = p5;
-    this.posX = 0;
-    this.posY = p5.random(-50, 0);
-    this.initialangle = p5.random(0, 2 * Math.PI);
-    this.size = p5.random(3, 7);
-
-    // radius of snowflake spiral
-    // chosen so the snowflakes are uniformly spread out in area
-    this.radius = Math.sqrt(this.p5.random(Math.pow(width / 2, 2)));
-  }
-  update(time) {
-    // x position follows a circle
-    let w = 0.1; // angular speed
-    let angle = w * time + this.initialangle;
-    this.posX = width / 2 + this.radius * Math.sin(angle);
-
-    // different size snowflakes fall at slightly different y speeds
-    this.posY += Math.pow(this.size, 0.5);
-  }
-
-  y() {
-    return this.posY;
-  }
-}
-
-class SnowFlake extends FallingObject {
-  display() {
-    this.p5.fill(this.p5.color(255, 255, 255));
-    this.p5.noStroke();
-    this.p5.ellipse(this.posX, this.posY, this.size);
-  }
-}
-
-class Character extends FallingObject {
-  constructor(p5, text) {
-    super(p5);
-    this.text = text;
-    this.size = this.p5.random(40, 60);  // TODO this should be passed as the constructor argument
-  }
-
-  display() {
-    this.p5.fill(255, 255, 255);
-    this.p5.textSize(this.size);
-    this.p5.text(this.text, this.posX, this.posY);
-  }
-}
-
 function animatePhrase(now, unit) {
   if (unit.contains(now)) {
     snowflakes.push(new Character(p5, unit.text));
@@ -146,7 +93,6 @@ function rewindVideo() {
   console.log("rewind button is clicked");
   player.video && player.requestMediaSeek(0);
 }
-
 
 // p5.js を初期化
 new P5((p5) => {
